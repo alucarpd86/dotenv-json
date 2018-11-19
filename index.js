97c09dc1c728e696347c38ab707730a9d508356a
Matteo Bruni <matteo.bruni@eudata.com>
2018-11-19 14:06:06 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 const path = require("path");
 const fs = require("fs");
 
+var opts = {
+    path: ".env.json",
+    override: false,
+    watch: false
+};
+
 module.exports = function dotenvJSON(options) {
-  const jsonFile = (options && options.path) || ".env.json";
+    opts = Object.assign(opts, options);
 
-  const jsonString = fs.readFileSync(path.resolve(process.cwd(), jsonFile), {
-    encoding: "utf8"
-  });
+    updateConfiguration();
 
-  try {
+    if (opts.watch) {
+        fs.watchFile(path.resolve(process.cwd(), opts.path), {interval: 1000}, (curr, prev) => {
+            updateConfiguration();
+        });
+    }
+};
+
+function updateConfiguration() {
+    const jsonString = fs.readFileSync(path.resolve(process.cwd(), opts.path), {
+        encoding: "utf8"
+    });
+
     const envConfig = JSON.parse(jsonString);
 
-    for (const key in envConfig) {
-      process.env[key] = process.env[key] || envConfig[key];
+    try {
+        for (const key in envConfig) {
+            if (opts.override) {
+                process.env[key] = envConfig[key];
+            } else {
+                process.env[key] = process.env[key] || envConfig[key];
+            }
+        }
+    } catch (err) {
+        console.error(err);
     }
-  } catch (err) {
-    console.error(err);
-  }
-};
+}
